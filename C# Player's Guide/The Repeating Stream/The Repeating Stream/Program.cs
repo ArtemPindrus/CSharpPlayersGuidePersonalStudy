c807a4ce6796d66daed3f9e06bb43aa0c750aaed
ArtemPindrus <artempindrus@gmail.com>
2024-02-13 13:03:55 +0200

    The Repeating Stream

---
+++ C# Player's Guide/The Repeating Stream/The Repeating Stream/Program.cs
@@ -0,0 +1,34 @@
+﻿using Extensions;
+
+namespace The_Repeating_Stream {
+    internal class Program {
+        static void Main() {
+            RecentNumbers recents = new();
+            Thread fountain = new(AssignRandomValues);
+            fountain.Start(recents);
+
+            while (true) { 
+                Console.ReadKey(true);
+
+                if (recents.NumbersAreEqual) ConsoleExtensions.WriteLineColor("Report is correct!", ConsoleColor.Green);
+                else ConsoleExtensions.WriteLineColor("Report is incorrect", ConsoleColor.Red);
+            }
+        }
+
+        private static void AssignRandomValues(object? recentNumbers) {
+            if (recentNumbers == null) throw new ArgumentNullException(nameof(recentNumbers));
+            else if (recentNumbers is not RecentNumbers) throw new ArgumentException("recentNumbers must be of RecentNumbers type!");
+
+            RecentNumbers recents = (RecentNumbers)recentNumbers;
+            Random random = new();
+
+            while (true) {
+                int number = random.Next(10);
+                recents.AddNumber(number);
+                Console.WriteLine(number);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ C# Player's Guide/The Repeating Stream/The Repeating Stream/RecentNumbers.cs
@@ -0,0 +1,25 @@
+﻿namespace The_Repeating_Stream {
+    class RecentNumbers {
+        private readonly object numbersLock = new();
+
+        private int index;
+        private readonly int[] numbers = new int[2];
+
+        public bool NumbersAreEqual {
+            get {
+                lock (numbersLock) { 
+                    return numbers[0] == numbers[1];
+                }
+            }
+        }
+
+        public void AddNumber(int number) {
+            lock (numbersLock) {
+                numbers[index] = number;
+                index++;
+
+                if (index == 2) index = 0;
+            }
+        }
+    }
+}
